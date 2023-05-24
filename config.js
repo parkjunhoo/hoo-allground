b646c348dc62bfb0d991ba8387ed5c297dcfa157
Hoo <56852562+parkjunhoo@users.noreply.github.com>
2023-05-24 19:41:06 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret': 'grounddhfrmfkdnsemall',
-    'mongodbUri': 'mongodb+srv://allground:thereisallgroundlevel11@cluster0.sqqja.mongodb.net/allground_db?retryWrites=true&w=majority'
+    'secret': 'ㅁㄴㅇㄻㄴㅇㄻㄴㅇㄹ',
+    'mongodbUri': 'ㅁㄴㅇㄹㄴㅁㅇㄹㄴㅁㅇㄹㄴㅇㅁㄹ'
 }
